9a4a52ec9d068c411aad9b92035ecf431a186e54
SUISSE DE SAINTE CLAIRE Hugo <hsuissedesa@micanon.pedago.ipb.fr>
2024-09-17 16:07:25 +0200

    champi au lieu de saucisses

--- PoussezMoiExcusezVous.c
+++ PoussezMoiExcusezVous.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
-  char aliment[10000] = "saucisse";
-  char quantite = 3;
+  char aliment[10000] = "champignon";
+  int quantite = 300;
   printf("Jadore %s quand y'en a %d !", aliment, quantite);
   return 0;
 }
