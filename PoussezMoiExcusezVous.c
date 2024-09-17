be2f7e80eaa80357d40c68d73d1f08585e5e2633
Hugo <hugodsc22@gmail.com>
2024-09-17 16:04:01 +0200

    Update PoussezMoiExcusezVous.c

--- PoussezMoiExcusezVous.c
+++ PoussezMoiExcusezVous.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main(){
-  char[10000] aliment = "saucisse";
+  char aliment[10000] = "saucisse";
   char quantite = 3;
   printf("Jadore %s quand y'en a %d !", aliment, quantite);
   return 0;
